94544076d315e0dcbc723fbf60266e2984bcfaa3
Vladislav <mypulse@yandex.ru>
2022-10-12 14:50:45 +0300

    name

---
+++ Example_All/Example_006NameIFelse/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine(); 
+
+if(username.ToLower() == "влад")
+{
+    Console.WriteLine("Ура, Это же Влад!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
+
+    
