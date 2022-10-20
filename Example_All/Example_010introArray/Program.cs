7ddd897145bc5de20e5c3d1478d603cf546d4dc7
Vladislav <mypulse@yandex.ru>
2022-10-20 10:42:04 +0300

    23

---
+++ Example_All/Example_010introArray/Program.cs
@@ -0,0 +1,90 @@
+﻿//1 Задача
+/*int Max (int arg1, int arg2, int arg3)
+{
+    
+    int result = arg1; // где result это max-значение
+    if(arg2 > result) result = arg2;
+    if(arg3 > result) result = arg3;
+    return result;
+}
+int[] array  = {11,211,31,41,15,61,17,18,19,};
+
+int result = Max (
+    Max(array[0], array[1], array[2]),
+    Max(array[3], array[4], array[5]),
+    Max(array[6], array[7], array[8])
+);
+Console.WriteLine(result);
+*/
+//2 задача
+/*
+int[] array = { 1, 12, 31, 4, 18, 15, 16, 17 ,18 };
+
+int n = array.Length;
+int find = 18;
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break; // прервать
+    }
+    index++;
+}
+*/
+//3 задача
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1,10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+} 
+
+//Задача 3 (Найти нужный элемент массива с помощью метода)
+
+int Index0f(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index =0;
+    int position = -1;
+
+    while (index < count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+array[4] = 4;
+array[6] = 4;
+PrintArray(array);
+Console.WriteLine();
+
+int pos = Index0f(array, 4);
+Console.WriteLine(pos);
